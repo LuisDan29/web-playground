3b744f80645a5fffe7cc29c0be2f5b19675bff82
Luis Dantas <luisdm5309@gmail.com>
2026-01-10 17:29:47 -0300

    sistemas de notas implementado - notas coloridas incompleto

--- my-grey-calendar/src/script.js
+++ my-grey-calendar/src/script.js
@@ -7,15 +7,102 @@ const MONTHS = [
 ];
 const MAIN_SECTION = document.getElementById('main');
 
+// localStorage.clear();
+const KEY_NAME = 'greycalendar-data';
+const CALENDAR_DATA = getCalendarData();
+
+let CURRENT_DAY = { yearmonth: '', day: 0 };
+const EDITOR = document.getElementById('editor');
+const TEXT_INPUT = document.getElementsByClassName('text-input')[0];
+
+
+function openNote(yearmonth, day) {
+    // mostra a div do editor
+    EDITOR.style.display = 'flex';
+    // salva as infos do dia sendo editado
+    CURRENT_DAY["yearmonth"] = yearmonth;
+    CURRENT_DAY["day"] = day;
+    // carrega texto existente da div
+    loadNoteText(yearmonth, day);   
+}
+
+
+function loadNoteText(yearmonth, day) {
+    const data = CALENDAR_DATA[yearmonth][day];
+    TEXT_INPUT.textContent = data;
+    if (!data) {
+        TEXT_INPUT.textContent = '';
+    }
+}
+
+
+function closeNote() {
+    EDITOR.style.display = 'none';
+    console.log(getCalendarData());
+}
+
+
+function saveNote() {
+    const yearmonth = CURRENT_DAY["yearmonth"];
+    const day = CURRENT_DAY["day"];
+    const text = TEXT_INPUT.textContent;
+    
+    // Garante que o mês existe no objeto
+    if (!CALENDAR_DATA[yearmonth]) {
+        CALENDAR_DATA[yearmonth] = {};
+    }
+    
+    CALENDAR_DATA[yearmonth][day] = text;
+    
+    // Envia alterações pro localStorage
+    setCalendarData();
+    closeNote();
+}
+
+
+function deleteNote() {
+    const yearmonth = CURRENT_DAY["yearmonth"];
+    const day = CURRENT_DAY["day"];
+    
+    delete CALENDAR_DATA[yearmonth][day];
+    
+    setCalendarData();
+    closeNote();
+}
+
+
+
+function getCalendarData() {
+    // procura dados no localStorage
+    const data = JSON.parse(localStorage.getItem(KEY_NAME)) || {};
+    // cria configuração inicial se nao existir
+    if (Object.keys(data).length === 0) {
+        for (let month = 0; month < 12; month++) {
+            let yearmonth = `${ YEAR }-${ month.toString().padStart(2, "0") }`;
+            data[yearmonth] = {};
+        }
+    }
+    return data;
+}
+
+
+function setCalendarData() {
+    // envia a constante CALENDAR_DATA para localStorage
+    const data = JSON.stringify(CALENDAR_DATA);
+    localStorage.setItem(KEY_NAME, data);
+}
+
+
+createCalendar();
+
 
 function createCalendar() {
+    // incializa div's para cada mes
     for (let month = 0; month < 12; month++) {
         const monthElement = createMonth(YEAR, month);
         MAIN_SECTION.appendChild(monthElement);
     }
 }
-
-createCalendar();
 
 
 function createMonth(year, month) {
@@ -63,16 +150,28 @@ function createGrid(year, month) {
 
 
 function createDay(year, month, day) {
+    const yearmonth = `${ YEAR }-${ month.toString().padStart(2, "0") }`;
+    
     const div = document.createElement('div');
-    div.classList.add('day');
+    div.classList.add('day', yearmonth, day);
+    
+    // ao clicar, abre o editor de nota
+    div.addEventListener('click', () => { 
+        openNote(yearmonth, day); 
+    });
     
     div.innerHTML = `<p>${day}</p>`;
+    
+    // calcula grau de inclinação aleatório para day::before
+    let degree = (Math.random() * 6 - 3).toFixed(2);
+    div.style.setProperty('--note-degree', `${degree}deg`);
     
     return div;
 }
 
 
 function createEmptyDiv() {
+    // cria div's vazias
     const div = document.createElement('div');
     div.classList.add('empty');
     return div;
